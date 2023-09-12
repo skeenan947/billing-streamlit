3dfa1ac1c95ad41a7e632ad21101cc4515806324
Shaun Keenan <skeenan@zencore.dev>
2023-09-12 12:56:39 -0700

    cleanup

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import os
 from langchain.llms import VertexAI #LlamaCpp
 from google.cloud import bigquery
 import logging
-import pandas
 
 os.environ['GOOGLE_CLOUD_PROJECT']='g-playground-1'
 
@@ -12,7 +11,7 @@ def process_prompt():
     user_query = st.session_state.user_query
     st.chat_message("user").write(user_query)
     dataset = 'g-playground-1.internal_billing_dataset.gcp_billing_export_v1_010767_AD0D5D_BCC8F6'
-    schema = ""
+
     with open("schema.json", "r") as f:
         schema = f.read()
 
@@ -20,16 +19,16 @@ def process_prompt():
         prompt = f.read()
     template = prompt.format(schema=schema, question=user_query, dataset=dataset)
 
-    # Use for Llama
+    # Use for Llama``
     # n_gpu_layers = 40  # Change this value based on your model and your GPU VRAM pool.
     # n_batch = 512  # Should be between 1 and n_ctx, consider the amount of VRAM in your GPU.
-    # Make sure the model path is correct for your system!
-    #llm = LlamaCpp(
+    # #Make sure the model path is correct for your system!
+    # llm = LlamaCpp(
     #    model_path="./llama-2-7b-chat.ggmlv3.q4_0.bin",
     #    n_gpu_layers=n_gpu_layers,
     #    n_batch=n_batch,
     #    verbose=True,
-    #)
+    # )
     llm = VertexAI(model_name="code-bison", max_output_tokens=2048)
     # in case it decides to spit out markdown-formatted SQL instead of straight SQL
     sql = llm(prompt = template).replace("```", "").replace("sql", "")
